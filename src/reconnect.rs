4ca5dc05b4846a89bc7f14587e6111c62dc3e7fa
Ben Merritt <blm768@gmail.com>
2020-08-14 22:52:26 -0700

    Join helper task when closing ReconnectingConnection

--- src/reconnect.rs
+++ src/reconnect.rs
@@ -1,6 +1,11 @@
 use std::{io::ErrorKind, mem, ops::DerefMut, sync::Arc, time::Duration};
 
-use tokio::{sync::Mutex, time::delay_for};
+use tokio::{
+	select,
+	sync::{Mutex, Notify},
+	task::JoinHandle,
+	time::delay_for,
+};
 
 use crate::{
 	connection::{Settings, SingleConnection},
@@ -14,6 +19,11 @@ enum Status {
 	Stopped,
 }
 
+struct Internal {
+	status: Mutex<Status>,
+	close_connection: Notify,
+}
+
 /// Drop-in replacement wrapper of [`Connection`](struct.Connection.html) which intercepts all [`IO errors`](enum.Error.html#variant.IO)
 /// returned by [`Connection::exec`](struct.Connection.html#method.exec) to start the reconnection thread, and will opt to return [`BusyReconnecting`](enum.Error.html#variant.BusyReconnecting)
 /// instead.
@@ -24,7 +34,8 @@ pub struct ReconnectingConnection {
 	pass: String,
 	settings: Settings,
 
-	internal: Arc<Mutex<Status>>,
+	internal: Arc<Internal>,
+	reconnect_loop: Option<JoinHandle<()>>,
 }
 
 impl ReconnectingConnection {
@@ -32,14 +43,19 @@ impl ReconnectingConnection {
 	pub async fn open(address: impl ToString, pass: impl ToString, settings: Settings) -> Result<Self, RconError> {
 		let address = address.to_string();
 		let pass = pass.to_string();
-		let internal = Arc::new(Mutex::new(Connected(
+		let status = Mutex::new(Connected(
 			SingleConnection::open(address.clone(), pass.clone(), settings.clone()).await?,
-		)));
+		));
+		let internal = Arc::new(Internal {
+			status,
+			close_connection: Notify::new(),
+		});
 		Ok(ReconnectingConnection {
 			address,
 			pass,
 			settings,
 			internal,
+			reconnect_loop: None,
 		})
 	}
 
@@ -48,7 +64,7 @@ impl ReconnectingConnection {
 	pub async fn exec(&mut self, cmd: impl ToString) -> Result<String, RconError> {
 		// First, we check if we are actively reconnecting, this must be done within a Mutex
 		let result = {
-			let mut lock = self.internal.lock().await;
+			let mut lock = self.internal.status.lock().await;
 			let connection = match lock.deref_mut() {
 				Connected(ref mut c) => c,
 				Disconnected(msg) => return Err(BusyReconnecting(msg.clone())),
@@ -73,39 +89,47 @@ impl ReconnectingConnection {
 	}
 
 	/// Closes the connection, joining any background tasks that were spawned to help manage it.
-	pub async fn close(self) {
-		let mut lock = self.internal.lock().await;
-		if let Connected(connection) = mem::replace(&mut *lock, Status::Stopped) {
-			connection.close().await;
+	pub async fn close(mut self) {
+		{
+			let mut lock = self.internal.status.lock().await;
+			if let Connected(connection) = mem::replace(&mut *lock, Status::Stopped) {
+				connection.close().await;
+			}
 		}
-		// TODO: if reconnecting, we probably want to make sure to join the background task (and terminate it early if it's still waiting for a connection).
+
+		self.internal.close_connection.notify();
+		if let Some(handle) = self.reconnect_loop.take() {
+			handle.await.unwrap_or_else(|e| match e.is_cancelled() {
+				true => (), // Cancellation is fine.
+				false => panic!(e.into_panic()),
+			});
+		}
 	}
 
-	async fn start_reconnect(&self, e: RconError) -> RconError {
+	async fn start_reconnect(&mut self, e: RconError) -> RconError {
 		// First, we change the status, which automatically disconnects the old connection
 		{
-			let mut lock = self.internal.lock().await;
+			let mut lock = self.internal.status.lock().await;
 			*lock = Disconnected(e.to_string());
 		}
 
-		tokio::spawn(Self::reconnect_loop(
+		self.reconnect_loop = Some(tokio::spawn(Self::reconnect_loop(
 			self.address.clone(),
 			self.pass.clone(),
 			self.settings.clone(),
 			self.internal.clone(),
-		));
+		)));
 
 		BusyReconnecting(e.to_string())
 	}
 
-	async fn reconnect_loop(address: String, pass: String, settings: Settings, internal: Arc<Mutex<Status>>) {
+	async fn reconnect_loop(address: String, pass: String, settings: Settings, internal: Arc<Internal>) {
 		loop {
-			match SingleConnection::open(address.clone(), pass.clone(), settings.clone()).await {
-				Err(_) => {
-					delay_for(Duration::from_secs(1)).await;
-				}
-				Ok(c) => {
-					let mut lock = internal.lock().await;
+			let close_connection = internal.close_connection.notified();
+			let connection = SingleConnection::open(address.clone(), pass.clone(), settings.clone());
+			select! {
+				Ok(c) = connection => {
+					let mut lock = internal.status.lock().await;
 					match *lock {
 						Stopped => c.close().await,
 						_ => {
@@ -113,8 +137,15 @@ impl ReconnectingConnection {
 						}
 					}
 					return;
-				}
-			}
+				},
+				_ = close_connection => return,
+				else => (), // Connection error
+			};
+			let close_connection = internal.close_connection.notified();
+			select! {
+				_ = delay_for(Duration::from_secs(1)) => (),
+				_ = close_connection => return,
+			};
 		}
 	}
 }
